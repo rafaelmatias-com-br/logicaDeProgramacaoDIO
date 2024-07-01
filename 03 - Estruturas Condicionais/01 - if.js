afa7d3d566e11e4a3ef01c0a12b458c600cd6ac1
Rafael Matias <rafael@matias.com>
2024-06-30 21:03:05 -0300

    Uploading the 3rd unity of my course

---
+++ 03 - Estruturas Condicionais/01 - if.js
@@ -0,0 +1,14 @@
+let itsOn = true
+
+if (itsOn) {
+    console.log("Está ligado!") //true
+}
+
+let hasEggs = false
+let cart = ""
+
+if (hasEggs) {
+    cart = "leite"
+}
+
+console.log("item comprado: " + cart)
